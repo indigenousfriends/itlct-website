824e18d05de516ba3ba8d490de1e2e1b7c5dddbb
maxharrisnet <hello@maxharris.codes>
2023-12-11 10:24:16 -0800

    adding conditional for events email

--- data/form.php
+++ data/form.php
@@ -2,13 +2,19 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $firstName = $_POST['first-name'];
     $lastName = $_POST['last-name'];
+    $destination = $_POST['contact-dropdown'];
     $email = $_POST['email'];
     $message = $_POST['description'];
 
     // $to = 'info@iftheselandscouldtalk.org';
     // $events = 'events@iftheselandscouldtalk.org';
     $test = 'max@indigenousfriends.org';
+    $testEvents = 'max+events@indigenousfriends.org';
     $to = $test;
+
+    if ($destination == 'events') {
+        $to = $testEvents;
+    }
 
     $cc = 'pam@indigenousfriends.org, solivan@indigenousfriends.org';
     $subject = 'New contact form message from the ITLCT website';
@@ -21,7 +27,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if(mail($to, $subject, $body, $headers)){
         echo 'Message sent successfully';
-    } else{
+    } else {
         echo 'Message sending failed';
     }
 }
