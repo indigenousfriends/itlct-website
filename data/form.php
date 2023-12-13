6e621932660a16e1b1185b6c257bfb5175e24cad
maxharrisnet <hello@maxharris.codes>
2023-12-13 07:34:28 -0800

    correcting variable for the destination in the form

--- data/form.php
+++ data/form.php
@@ -4,7 +4,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $lastName = $_POST['last-name'];
     $name = $firstName . " " . $lastName;
     $email = $_POST['email'];
-    $destination = $_POST['contact-dropdown'];
+    $destination = $_POST['destination'];
     $message = $_POST['description'];
 
     // $info = 'info@iftheselandscouldtalk.org';
@@ -29,7 +29,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 
     $subject = "New $regarding message from the ITLCT website form";
-    $headers = "From: If These Lands Could Talk Webform <$from>" . "\r\n" .
+    $headers = "From: ITLCT website form <$from>" . "\r\n" .
         "Reply-To: $name <$email>" . "\r\n" .
         "CC: $cc" . "\r\n" .
         "MIME-Version: 1.0\r\n" .
@@ -39,7 +39,35 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $body = "You have received a new message from the contact form on the If These Lands Could Talk website regarding $events.\n\n" . "Here are the details:\n\nName: $name\n\nEmail: $email\n\nMessage:\n$message";
 
     if (mail($to, $subject, $body, $headers)) {
-        header('Location: ' . $_SERVER['HTTP_REFERER'] . '?success=true');
+        header('Location: ' . $_SERVER['HTTP_REFERER'] . '#contact-us-form?success=true');
+        exit;
+    } else {
+        echo 'Message sending failed';
+    }
+}
+
+    if ($destination == 'events') {
+        $to = $testEvents;
+        $cc = $testInfo;
+        $regarding = 'events';
+    } elseif ($destination == 'join') {
+        $regarding = 'joining the team';
+    } else {
+        $regarding = 'general';
+    }
+
+    $subject = "New $regarding message from the ITLCT website form";
+    $headers = "From: ITLCT website form <$from>" . "\r\n" .
+        "Reply-To: $name <$email>" . "\r\n" .
+        "CC: $cc" . "\r\n" .
+        "MIME-Version: 1.0\r\n" .
+        "Content-type: text/html; charset=iso-8859-1\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+
+    $body = "You have received a new message from the contact form on the If These Lands Could Talk website regarding $events.\n\n" . "Here are the details:\n\nName: $name\n\nEmail: $email\n\nMessage:\n$message";
+
+    if (mail($to, $subject, $body, $headers)) {
+        header('Location: ' . $_SERVER['HTTP_REFERER'] . '#contact-us-form?success=true');
         exit;
     } else {
         echo 'Message sending failed';
