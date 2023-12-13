47bb63ee05b88b928efaae7d518c8e18e8415712
maxharrisnet <hello@maxharris.codes>
2023-12-13 09:04:11 -0800

    form - duplicate code, successURL variable

--- data/form.php
+++ data/form.php
@@ -18,32 +18,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $to = $test;
     $cc = "";
 
-    if ($destination == 'events') {
-        $to = $testEvents;
-        $cc = $testInfo;
-        $regarding = 'events';
-    } elseif ($destination == 'join') {
-        $regarding = 'joining the team';
-    } else {
-        $regarding = 'general';
-    }
-
-    $subject = "New $regarding message from the ITLCT website form";
-    $headers = "From: ITLCT website form <$from>" . "\r\n" .
-        "Reply-To: $name <$email>" . "\r\n" .
-        "CC: $cc" . "\r\n" .
-        "MIME-Version: 1.0\r\n" .
-        "Content-type: text/html; charset=iso-8859-1\r\n" .
-        'X-Mailer: PHP/' . phpversion();
-
-    $body = "You have received a new message from the contact form on the If These Lands Could Talk website regarding $events.\n\n" . "Here are the details:\n\nName: $name\n\nEmail: $email\n\nMessage:\n$message";
-
-    if (mail($to, $subject, $body, $headers)) {
-        header('Location: ' . $_SERVER['HTTP_REFERER'] . '#contact-us-form?success=true');
-        exit;
-    } else {
-        echo 'Message sending failed';
-    }
+    $successURL = $_SERVER['HTTP_REFERER'] . '?success=true#contact-us-form';
 
     if ($destination == 'events') {
         $to = $testEvents;
@@ -66,7 +41,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $body = "You have received a new message from the contact form on the If These Lands Could Talk website regarding $events.\n\n" . "Here are the details:\n\nName: $name\n\nEmail: $email\n\nMessage:\n$message";
 
     if (mail($to, $subject, $body, $headers)) {
-        header('Location: ' . $_SERVER['HTTP_REFERER'] . '?success=true#contact-us-form');
+        header('Location: ' . $successURL);
         exit;
     } else {
         echo 'Message sending failed';
